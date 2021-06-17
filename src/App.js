9ecad985b685ffaed731a54cebd932b53473b2f4
FGM75 <fgonzalezm75@gmail.com>
2021-06-17 17:45:20 +0200

    carga bootstrap y html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>Hola mundo!</div>;
+  return (
+    <div className="container-principal">
+      <h1>Listado Crud</h1>
+      <div className="row">
+        <div className="columna-añadir col">
+          <h2>Añadir</h2>
+        </div>
+        <div className="columna-estado col">
+          <h2>Estado</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
